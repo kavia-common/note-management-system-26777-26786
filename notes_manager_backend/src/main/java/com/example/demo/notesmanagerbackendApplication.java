753d42dca50cc0cd1165c11c17fb456499c83c60
Kavia AI <production@kavia.ai>
2025-09-26 11:28:46 +0000

    CheckPoint - cg23ad7f69

---
+++ notes_manager_backend/src/main/java/com/example/demo/notesmanagerbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.notesmanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class notesmanagerbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(notesmanagerbackendApplication.class, args);
+	}
+
+}
---
+++ notes_manager_backend/src/test/java/com/example/demo/notesmanagerbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notesmanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class notesmanagerbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
